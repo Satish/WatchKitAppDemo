49f4eeaee3275e99fe5a363563812a616a328a06
Satish Chauhan <satishchauhan80@gmail.com>
2014-12-10 16:50:31 +0530

    Watch kit 'hello world' app

---
+++ WatchKitAppDemo WatchKit Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  WatchKitAppDemo WatchKit Extension
+//
+//  Created by Satish Chauhan on 10/12/14.
+//  Copyright (c) 2014 Vinsol. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
